a87534730f18b9a9a5c6e2934238e0f9463a20b6
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2026-01-19 00:42:37 +0530

    update

--- src/main/java/com/akashpatel/usermanagement/repository/UserRepository.java
+++ src/main/java/com/akashpatel/usermanagement/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.akashpatel.usermanagement.repository;
 
+import com.akashpatel.usermanagement.dto.UserRequestDto;
+import com.akashpatel.usermanagement.dto.UserResponseDto;
 import com.akashpatel.usermanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
 }
--- src/main/java/com/akashpatel/usermanagement/services/IUserService.java
+++ src/main/java/com/akashpatel/usermanagement/services/IUserService.java
@@ -1,5 +1,8 @@
 package com.akashpatel.usermanagement.services;
 
+import com.akashpatel.usermanagement.dto.UserRequestDto;
+import com.akashpatel.usermanagement.dto.UserResponseDto;
+
 public interface IUserService {
 
 }
