d35f05b37a99ecf78a0f5cdfb9e24251e5572ccd
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2026-01-19 00:21:38 +0530

    validation added

--- src/main/java/com/akashpatel/usermanagement/dto/UserRequestDto.java
+++ src/main/java/com/akashpatel/usermanagement/dto/UserRequestDto.java
@@ -1,5 +1,7 @@
 package com.akashpatel.usermanagement.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,9 +12,15 @@ import lombok.Setter;
 @Builder
 
 public class UserRequestDto {
+    @NotBlank(message = "Name Required ")
     private String name;
+    @Email
+    @NotBlank(message = "Enter email")
     private String email;
+
+    @NotBlank(message = "Please enter age")
     private int age;
+    @NotBlank(message = "Male/Female/Others")
     private String gender;
 
 }
