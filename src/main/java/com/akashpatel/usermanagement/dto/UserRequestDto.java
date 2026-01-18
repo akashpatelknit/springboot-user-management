ec188aaf61207ea06350eae8f45a8c32aa6b82ae
shivangi_singh <93123654+shivangi462@users.noreply.github.com>
2026-01-18 23:55:21 +0530

    responseDTO

--- src/main/java/com/akashpatel/usermanagement/dto/UserRequestDto.java
+++ src/main/java/com/akashpatel/usermanagement/dto/UserRequestDto.java
@@ -1,4 +1,5 @@
 package com.akashpatel.usermanagement.dto;
 
 public class UserRequestDto {
+
 }
--- src/main/java/com/akashpatel/usermanagement/dto/UserResponseDto.java
+++ src/main/java/com/akashpatel/usermanagement/dto/UserResponseDto.java
@@ -1,4 +1,11 @@
 package com.akashpatel.usermanagement.dto;
 
+import java.util.UUID;
+
 public class UserResponseDto {
+    private UUID id;
+    private String name;
+    private String email;
+    private int age;
+    private String gender;
 }
